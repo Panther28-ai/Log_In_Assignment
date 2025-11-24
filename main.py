d9322bc259693a571863e18c9b804bc447ee8942
Mikhail Gray <mikhailjgray@gmail.com>
2025-11-24 16:23:15 +0000

    login functionality with attempts tracking and main window creation after succesfull log in

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 from customtkinter import *
 import csv
 
+from tkinter import messagebox, ttk
+attempts_left = 3
+
+
+def login_success():
+    # create window after login main screen
+    main = CTk(fg_color="#909590")
+    main.geometry("1920x1080")
+    main.title("Trail Talk")
+    main.resizable(False, False)
+    main.attributes ("-fullscreen", True)
+    main.update()
+
+    main_exit_button = CTkButton(main, text="X", font=CTkFont(size=20, weight="bold"), fg_color="#FF5C5C", command=main.destroy)
+    main_exit_button.pack(side="top", anchor="ne", padx=10, pady=10)
+
+
+
+# log in function to check username and password against CSV file
+def login(username, password):
+    global attempts_left # to modify the global variable
+    with open('users.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader: # seach through each row
+            if row['username'] == username and row['password'] == password: # if both are equal it will work
+                login_success() # open main window 
+                exit_app() # kils the log in welcom window
+                return True # if this isnt here then messagebox will still show idk why 
+            else:     
+                attempts_left = attempts_left - 1 # needs to before the if statement or closes at -1
+                messagebox.showerror("Login Failed", f"Invalid username or password. You have {attempts_left} attempts left.")
+                if attempts_left <= 0:
+                    app.destroy()  # close the app if no attempts left
+                return False 
+
+# signup function to add new username and password to CSV file
+
+def signup(username, password):
+    with open('users.csv', mode='a', newline='') as file: # append mode to add without deleting
 
 
 
@@ -34,8 +73,11 @@ def open_login_signup():
     password_login_label.pack(pady=10)
     password_login_entry = CTkEntry(login_tab, width=300, font=CTkFont(size=20))
     password_login_entry.pack(pady=10)
-    login_button = CTkButton(login_tab, text="Log in", font=CTkFont(size=20), fg_color="#9AE19D")
+    login_button = CTkButton(login_tab, text="Log in", font=CTkFont(size=20), fg_color="#9AE19D", command=lambda: login(username_login_entry.get(), password_login_entry.get()))
     login_button.pack(pady=20)
+    #attemps left label
+    attempts_left_label = CTkLabel(login_tab, text="You have 3 total attempts", font=CTkFont(size=15), text_color="#FF0000")
+    attempts_left_label.pack(pady=5)
     
     # signup
     # username: label then entry Password: label then entry then button
@@ -48,23 +90,24 @@ def open_login_signup():
     password_signup_label.pack(pady=10)
     password_signup_entry = CTkEntry(signup_tab, width=300, font=CTkFont(size=20))
     password_signup_entry.pack(pady=10)
-    signup_button = CTkButton(signup_tab, text="Sign up", font=CTkFont(size=20), fg_color="#9AE19D")
+    signup_button = CTkButton(signup_tab, text="Sign up", font=CTkFont(size=20), fg_color="#9AE19D", command=lambda: signup(username_signup_entry.get(), password_signup_entry.get()))
     signup_button.pack(pady=20)
     
 #function to exit kill welcome window and app
 def exit_app():
     app.destroy()    
-    
+#def exit_main():
+#    main.destroy()
 
-#create the main window
+#create the login window
 app = CTk(fg_color="#909590")
 app.geometry("1920x1080")
-app.title("Trail Talk")
+app.title("Trail Talk | Login required")
 app.resizable(False, False)
 app.attributes ("-fullscreen", True)
 app.update()
 
-# main window Title
+# login window Title
 # exit button top right
 exit_button = CTkButton(app, text="X", font=CTkFont(size=20, weight="bold"), fg_color="#FF5C5C", command=app.destroy)
 exit_button.pack(side="top", anchor="ne", padx=10, pady=10)
@@ -85,7 +128,6 @@ Open_login_window_button.pack(pady=90)
 
 
 
-
 #run the application
 app.mainloop()
 
@@ -93,5 +135,5 @@ app.mainloop()
 """
 TO do list:
 - Make sure only one login/signup window has one instance at a time
-
+- stuck on appending to CSV file 
 """ 
